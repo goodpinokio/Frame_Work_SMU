122b258e8d2329d54bfd48d927293657bf4f2db7
goodpinokio <magic110612@gmail.com>
2023-04-10 18:37:41 +0900

    blog 웹 페이지 완성

---
+++ venv/bin/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.single_post_page),
+]
--- venv/bin/blog/views.py
+++ venv/bin/blog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Post
 
 # Create your views here.
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
+
+class PostList(ListView): #모델명_list.hmtl
+    model = Post #post_list 변수
+    # template_name = 'blog/post_list.html' 템플릿 이름 바꾸기
+    ordering = '-pk'
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+
+        request,
+        'blog/single_post_page.html',
+        {
+            'post':post,
+        }
+    )
